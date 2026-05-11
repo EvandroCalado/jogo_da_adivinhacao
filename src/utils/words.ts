bd71b7804de4f2700eab78259bfd856ba5289f53
Evandro Calado <evandrocalado07@gmail.com>
2026-05-11 09:44:02 -0300

    init: initialize project

---
+++ src/utils/words.ts
@@ -0,0 +1,28 @@
+export type Challenger = {
+  id: number;
+  word: string;
+  tip: string;
+};
+
+export const WORDS: Challenger[] = [
+  {
+    id: 1,
+    word: "cachorro",
+    tip: "Animal doméstico muito popular",
+  },
+  {
+    id: 2,
+    word: "gato",
+    tip: "Animal doméstico muito popular",
+  },
+  {
+    id: 3,
+    word: "pássaro",
+    tip: "Animal que voa",
+  },
+  {
+    id: 4,
+    word: "peixe",
+    tip: "Animal que vive na água",
+  },
+];
